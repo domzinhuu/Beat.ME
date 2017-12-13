a9ed25a56c2a7102cd3325df0eb13b5e84552276
Maique Rosa <maique.rosa@gmail.com>
2017-12-13 20:55:23 -0200

    correções finais

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 4200)
+app.listen(process.env.PORT || 4200);
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
